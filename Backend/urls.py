e4f6a0263c501dec0e6e9e9e28af3f0c34a5f4ad
Dilan Redha <dilanredha@hotmail.com>
2022-11-01 13:52:06 +0100

    Added User simple table and django rest framework for future use

--- Backend/urls.py
+++ Backend/urls.py
@@ -14,8 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.models import User 
+
 from django.urls import path
+from django.conf.urls import include 
+from rest_framework import routers, serializers, viewsets
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-path/', include('rest_framework.urls'))
 ]
