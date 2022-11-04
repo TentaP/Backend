7f55745d4fb5da271431fcb19c7877083ebf6b76
noorbhtiti <nour.bahtite@gmail.com>
2022-11-04 16:06:04 +0100

    Update models.py

--- tentap/models.py
+++ tentap/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class Users(models.Model):
@@ -7,6 +8,33 @@ class Users(models.Model):
     password = models.CharField(max_length=200)
     is_admin = models.BooleanField(default=False)
     is_super_user = models.BooleanField(default=False)
+    # university_name = models.CharField(max_length=200) TODO
 
 
+# Files model skeleton
+"""
+class Files(models.Model):
+    file_name = models.CharField(max_length=200)
+    uploaded_by = Users.user_name  # change to be related to the user
+    course_name = models.CharField(max_length=200)
+    university_name = models.CharField(max_length=200)  # change to be related to the university
+    date_of_uploading = models.DateTimeField(auto_now=True)
+"""
 
+# University model skeleton
+"""
+class University(models.Model):
+    university_name = models.CharField(max_length=200,unique=True)
+    courses = ArrayField(
+        models.CharField(max_length=200)
+    )
+"""
+
+
+# Courses model skeleton
+"""
+class Courses(models.Model):
+    course_name = models.CharField(max_length=200)
+    at_university = models.CharField(max_length=200) # TODO change to be related to university model
+    Files = models.CharField(max_length=200) # TODO change to be related to files model 
+"""
