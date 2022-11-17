9c7877056f369f6d57cb30b9a4a1f384a138c919
Dilan Redha <dilanredha@hotmail.com>
2022-11-17 16:43:36 +0100

    Correct relations TODO test delete policy

--- tentap/models.py
+++ tentap/models.py
@@ -23,19 +23,20 @@ class UserSession(models.Model):
 class File(models.Model):
     # Types of files, subject to change.
     file_name = models.CharField(max_length=200)
-    uploaded_by = models.ForeignKey('User', on_delete=models.PROTECT)
+    uploaded_by = models.ForeignKey('User', related_name="Files",  on_delete=models.PROTECT)
 
-    file = models.FileField(upload_to='documents/%Y/%m/%d', blank=True, null=True)
+    file = models.FileField(upload_to='documents/', blank=True, null=True)
+    image = models.ImageField(upload_to='documents/', blank=True, null=True)
+
+    course = models.ForeignKey('Course', on_delete=models.PROTECT)
+    at_university = models.ForeignKey('University', related_name="Files", on_delete=models.PROTECT)    
+    date_of_uploading = models.DateTimeField(auto_now=True)
+    reviews = models.ForeignKey('Review', related_name='File', on_delete=models.CASCADE, blank=True, null=True)
 
     class fileType(models.TextChoices):
         EX = 'EX', 'Exam'
         AS = 'AS', 'ASSIGNMENT'
         LB = 'LB', 'Lab'
-
-    course = models.ForeignKey('Course', on_delete=models.PROTECT)
-    at_university = models.ForeignKey('University', on_delete=models.PROTECT)    
-    date_of_uploading = models.DateTimeField(auto_now=True)
-    reviews = models.ForeignKey('Review', related_name='reviews', on_delete=models.CASCADE, blank=True, null=True)
     file_type = models.CharField(
         max_length=2,
         choices=fileType.choices,
@@ -44,16 +45,15 @@ class File(models.Model):
 
 class University(models.Model):
     university_name = models.CharField(max_length=200,unique=True)
-    courses = models.ForeignKey('Course', related_name='Courses', on_delete=models.CASCADE)
+    courses = models.ForeignKey('Course', related_name='University', on_delete=models.CASCADE)
 
 class Course(models.Model):
     course_name = models.CharField(max_length=200, unique=True)
-    at_university = models.ForeignKey('University', on_delete=models.RESTRICT) 
-    files = models.ForeignKey('File', related_name='files', on_delete=models.CASCADE, blank=True, null=True)
+    files = models.ForeignKey('File', related_name='Course', on_delete=models.CASCADE, blank=True, null=True)
 
 
 class Review(models.Model):
-    author = models.ForeignKey('User',related_name='Author', on_delete=models.PROTECT)
+    author = models.ForeignKey('User',related_name='Reviews', on_delete=models.PROTECT)
     review = models.DecimalField(decimal_places=1, max_digits=1)
     body = models.CharField(max_length=700)
 
