7d4ace77f447b8c82ff9b6409c594428ea441cb2
Dilan Redha <dilanredha@hotmail.com>
2022-11-29 20:01:16 +0100

    file upload

--- tentap/api/file.py
+++ tentap/api/file.py
@@ -3,36 +3,99 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.parsers import JSONParser
+from rest_framework.parsers import JSONParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from django import forms
 
-from tentap.serializers import FileSerializer, FileForm
-from tentap.models import File
+from tentap.serializers import FileSerializer
+from tentap.models import File, Course, User
 from tentap.permissions import *
 
 """
 post request for the File model
 """
-class fileupload(APIView):
-    def post(self, request):
-        form = FileForm(request.POST, request.FILES)
-        #file_serializer = FileSerializer(data=file_data)
-        if form.is_valid():
-            instance = File(file=request.FILES['file'])
-            instance.save()
-            return JsonResponse(instance.data, status.HTTP_201_CREATED) 
-        return JsonResponse(form.errors, status.HTTP_400_BAD_REQUEST)
-    def get(self, request):
-        files = File.objects.all()
-        file_serializer = FileSerializer(files, many=True)
-        return JsonResponse(file_serializer.data, safe=False)
+class fileUpload(APIView):
+    permission_classes = [isNormalUser | isAdminUser | isSuperUser]
+    parser_classes = [MultiPartParser]
+    serializer_class = FileSerializer
 
-@api_view(['GET'])
-def file(request, pk):
-    files = File.objects.get(pk=pk)
-    if request.method == 'GET':
-        file_serializer = FileSerializer(file, many=False)
-        return JsonResponse(file_serializer.data, safe=False)
+    def post(self, request):
+        user = get_user(request)
+        #post = JSONParser().parse(request.POST)
+        request.data['uploaded_by'] = user.pk
+        request.data['at_university'] = int(request.data['at_university'])
+        request.data['course'] = int(request.data['course'])
+        serializer = self.serializer_class(data=request.data)
+        #file_serializer = FileSerializer(data=file_data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status.HTTP_201_CREATED) 
+        print (serializer.errors)
+        return JsonResponse(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+class filepk(APIView):
+    permission_classes = [isNormalUser | isAdminUser | isSuperUser]
+
+    def get(self, request, pk):
+        try:
+            file = File.objects.get(pk=pk)
+            serializer = FileSerializer(file)
+            return Response(serializer.data)
+        except File.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    """
+    change file if normaluser is the author of the file
+    Admin and Superuser can edit any file
+    """
+    def put(self, request, pk):
+        if isNormalUser and get_user(request) != file.author:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        try:
+            data = JSONParser().parse(request)
+            file = File.objects.get(pk=pk)
+            serializer = FileSerializer(file, data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status.HTTP_201_CREATED)
+            return JsonResponse(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        except File.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk):
+        try:
+            file = File.objects.get(pk=pk)
+            file.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except File.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+class filesByCourse(APIView):
+    permissions_classes = [isNormalUser, isAdminUser, isSuperUser]
+
+    def get(self, request, course):
+        try:
+            course = Course.objects.get(course_name=course)
+            files = course.files.all()
+            file_serializer = FileSerializer(files, many=True)
+            return JsonResponse(file_serializer.data, safe=False)
+        except Course.DoesNotExist:
+            return JsonResponse(status=status.HTTP_404_NOT_FOUND)
+
+class filesByUser(APIView):
+    permissions_classes = [isNormalUser, isAdminUser, isSuperUser]
+
+    def get(self, request):
+        try:
+            user = get_user(request)
+            try:
+                files = user.Reviews.all()
+                file_serializer = FileSerializer(files, many=True)
+                return JsonResponse(file_serializer.data, safe=False)
+            except File.DoesNotExist:
+                return JsonResponse(status=status.HTTP_404_NOT_FOUND)
+        except User.DoesNotExist:
+            return JsonResponse(status=status.HTTP_404_NOT_FOUND)
