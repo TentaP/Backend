3a70ed4675865a920aec3c6bcd451e59f09d13ad
noorbhtiti <nour.bahtite@gmail.com>
2022-12-06 16:01:27 +0100

    Update auth.py
    Auth is almost done>>
    TODO:
    Rerequest emailVerification link
    Status code fix

--- tentap/api/auth.py
+++ tentap/api/auth.py
@@ -1,6 +1,5 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 
-import jwt
 from django.utils import timezone
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -41,7 +40,7 @@ class signup(APIView):
         activation_link_payload = {
             "user": user_serializer.data['id'],
             "hash": hash_,
-            "expiry_data": timezone.now() + timedelta(minutes=3)  # TODO change this (hours=24)
+            "expiry_data": timezone.now() + timedelta(minutes=60)
         }
         activationLink_serializer = ActivationLinkSerializer(data=activation_link_payload)
         activationLink_serializer.is_valid(raise_exception=True)
@@ -93,47 +92,29 @@ class login(APIView):
         return resp
 
 
-class logout(APIView):
-    permission_classes = [isNormalUser | isAdminUser | isSuperUser]
-
-    def post(self, request):
-        token = request.headers.get("Authorization")
-        if not token:
-            raise NotAuthenticated("not authenticated!")
-        resp = Response()
-        resp.delete_cookie('jwt')
-        resp.data = {
-            "detail": "successfully logged out"
-        }
-        resp.status_code = 200
-        return resp
+# class logout(APIView):
+#     permission_classes = [isNormalUser | isAdminUser | isSuperUser]
+#
+#     def post(self, request):
+#         token = request.headers.get("Authorization")
+#         if not token:
+#             raise NotAuthenticated("not authenticated!")
+#         resp = Response()
+#         resp.delete_cookie('jwt')
+#         resp.data = {
+#             "detail": "successfully logged out"
+#         }
+#         resp.status_code = 200
+#         return resp
 
 
 # TODO move this to a new view
-class userView(APIView):
-    permission_classes = [isNormalUser | isAdminUser | isSuperUser]
-
-    def get(self, request):
-        token = request.headers.get("Authorization")
-
-        if not token:
-            raise NotAuthenticated("not authenticated!")
-
-        try:
-            payload = jwt.decode(token, secret.SECRET_KEY, algorithms="HS256")
-        except jwt.ExpiredSignatureError:
-            raise NotAuthenticated("not authenticated!")
-
-        user = User.objects.filter(id=payload["user_id"]).first()
-        serializer = UsersSerializer(user)
-        return Response(serializer.data)
-
 
 class emailVerification(APIView):
     def get(self, request, email: str, hash_: str):
         user = User.objects.filter(email=email.lower()).first()
         if not user:
-            return Response({"detail": ""}, status=500)  # TODO
+            return Response({"detail": "user not found!"}, status=500)  # TODO
 
         if user.is_active:
             return Response({"detail": "account is already active!"}, status=500)
@@ -163,14 +144,7 @@ class setSuperUser(APIView):
             raise NotAcceptable(f"{username} is already a super_user")
         user.is_superuser = True
         user.is_admin = True
-        try:
-            user.save()
-        except Exception:
-            raise {
-                "status_code": 500,
-                "detail": "an error occurred while saving in the database "
-            }
-
+        user.save()
         return Response({"detail": f"{username} is superuser now!"}, status=201)
 
 
@@ -186,13 +160,7 @@ class setAdmin(APIView):
         if user.is_admin:
             raise NotAcceptable(f"{username} is already an admin")
         user.is_admin = True
-        try:
-            user.save()
-        except Exception:
-            raise {
-                "status_code": 500,
-                "detail": "An Error occurred while saving in the database"
-            }
+        user.save()
         return Response({"detail": f"{username} is admin now!"}, status=201)
 
 
@@ -208,13 +176,7 @@ class removeAdmin(APIView):
         if not user.is_admin:
             raise NotAcceptable(f"{username} is not an admin")
         user.is_admin = False
-        try:
-            user.save()
-        except Exception:
-            raise {
-                "status_code": 500,
-                "detail": "an error occurred while saving in the database "
-            }
+        user.save()
         return Response({"detail": f"{username} is removed as an admin now!"}, status=200)
 
 
@@ -307,49 +269,3 @@ class resetPassword(APIView):
                 "detail": "an error occurred while saving in the database "
             }
         return Response({"detail": f"password for {user.email} has been updated"}, status=201)
-
-
-@csrf_exempt
-def users_list(request):
-    """
-    List all users, or create a new user.
-    """
-    if request.method == 'GET':
-        users = User.objects.all()
-        serializer = UsersSerializer(users, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = UsersSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse({"TEST": "DDF"}, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-
-@csrf_exempt
-def user_details(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        user = User.objects.get(pk=pk)
-    except User.objects.get(pk=pk).DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = UsersSerializer(user)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = UsersSerializer(user, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        user.delete()
-        return HttpResponse(status=204)
